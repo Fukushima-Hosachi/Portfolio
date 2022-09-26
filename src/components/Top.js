dd081a15b113cc5fbf64c9613f3869b014257168
Fukushima <c0p31078@bunkyo.ac.jp>
2022-09-26 09:22:16 +0900

    fix

--- src/components/Top.js
+++ src/components/Top.js
@@ -1,12 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-Y2WZH1K8KT"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-Y2WZH1K8KT');
-</script>
 
 import React from 'react'
 import Card from './Card';
